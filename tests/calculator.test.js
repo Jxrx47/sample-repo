3ffdf6eb60c3eb4ce8a9a0b354c44d6dd5172b8a
Jiraphan denphan <jiraphan.d@ku.th>
2025-12-25 15:08:46 +0700

    new-update-repo

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,7 +5,7 @@ describe('Calculator', () => {
         expect(add(2, 3)).toBe(5);
     });
 
-    test('adds -2 + -3 to equal 5', () => {
+    test('adds -2 + -3 to equal -5', () => {
         expect(add(-2, -3)).toBe(-5);
     });
 
@@ -14,11 +14,10 @@ describe('Calculator', () => {
     });
 
     test('multiplies 4 * 3 to equal 12', () => {
-        expect(multiply(4, 3)).toBe(12);npm 
+        expect(multiply(4, 3)).toBe(12);
     });
 
     test('divides 10 / 2 to equal 5', () => {
         expect(divide(10, 2)).toBe(5);
     });
 });
-////////////////////////////////////
